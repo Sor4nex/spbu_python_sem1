0566e58d125b76a03427bbc23720f2a9ddd26957
Aleksei Ivanov <st117366@student.spbu.ru>
2023-09-13 22:06:28 +0300

    Task №2 is now working as it should. File 3.py was deleted, because was added by accident

--- src/practice/pair_1/2.py
+++ src/practice/pair_1/2.py
@@ -1,18 +1,17 @@
 def rearrange_array() -> None:
     usr_inp = input('Введите целые числа, элементы массива, через пробел: ')
-    givven_array = [int(elem) if elem.isdigit() else None for elem in usr_inp.split(' ')]
-    while None in givven_array or len(givven_array) < 3:
+    given_array = [int(elem) if elem.isdigit() else None for elem in usr_inp.split(' ')]
+    while None in given_array or len(given_array) < 3:
         usr_inp = input('Один или несколько элементов не являются целыми числами, введите числа через пробел: ')
-        givven_array = [int(elem) if elem.isdigit() else None for elem in usr_inp.split(' ')]
+        given_array = [int(elem) if elem.isdigit() else None for elem in usr_inp.split(' ')]
     m_index = input('Введите индекс окончания среза: ')
-    while not m_index.isdigit() or int(m_index) > len(givven_array) - 1:
+    while not m_index.isdigit() or int(m_index) > len(given_array) - 1:
         m_index = input('Индекс не является числом, введите целое число: ')
-    m_index = int(m_index) - 1
-    givven_array.reverse()
-    givven_array[:m_index], givven_array[m_index:] = givven_array[m_index:], givven_array[:m_index]
-    givven_array.reverse()
-    print("Результат:", givven_array)
-
+    m_index = int(m_index)
+    given_array.reverse()
+    given_array[:-m_index], given_array[-m_index:] = given_array[-m_index:], given_array[:-m_index]
+    given_array.reverse()
+    print("Результат:", given_array)
 
 
 if __name__ == '__main__':
--- src/practice/pair_1/3.py
+++
@@ -1,5 +0,0 @@
-x = [1, 2, 3, 4, 5, 6]
-x.reverse()
-x[:3], x[3:] = x[3:], x[:3]
-x. reverse()
-print(x)
