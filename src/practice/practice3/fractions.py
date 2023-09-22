6659be20651139efe329f374a44d7f76a4132dcf
Aleksei Ivanov <st117366@student.spbu.ru>
2023-09-22 14:04:26 +0300

    Add solution to the 1 task (Дроби) of 3 practice

---
+++ src/practice/practice3/fractions.py
@@ -0,0 +1,28 @@
+def get_max_divisor() -> int:
+    max_divisor = input('Введите максимальный знаменатель дроби: ')
+    while not max_divisor.isdigit():
+        max_divisor = input('Ошибка. Введите число: максимальный знаменатель дроби: ')
+    return int(max_divisor)
+
+
+def find_greatest_common_divisor(first_number: int, second_number: int) -> int:
+    while first_number % second_number != 0:
+        first_number, second_number = second_number, first_number % second_number
+    return second_number
+
+
+def find_all_prime_fracions(max_divisor: int) -> list:
+    prime_fractions = list()
+    for divisor in range(1, max_divisor + 1):
+        for dividend in range(1, divisor):
+            if find_greatest_common_divisor(divisor, dividend) == 1:
+                prime_fractions.append((dividend, divisor))
+    return prime_fractions
+
+
+if __name__ == '__main__':
+    max_divisor = get_max_divisor()
+    prime_fractions = find_all_prime_fracions(max_divisor)
+    print(f'Простые несократимые дроби со знаменателем, не превыщающим {max_divisor}:')
+    for fraction in prime_fractions:
+        print(f'{fraction[0]}/{fraction[1]}', end=' ; ')
