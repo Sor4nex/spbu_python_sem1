33fb2cfbe03984d1f6d4661aa3a59547b7f84bc8
Aleksei Ivanov <st117366@student.spbu.ru>
2023-10-07 00:31:00 +0300

    Requested changes added.

--- src/homeworks/practice3/curry.py
+++ src/homeworks/practice3/curry.py
@@ -1,31 +1,31 @@
 def curry_explicit(function_to_curry, function_arity, given_args=None):
-    given_args = list() if given_args is None else given_args
+    if given_args is None:
+        given_args = []
     if function_arity < 0:
-        print("Error: Wrong function arity")
-        return None
+        return None  # Error: wrong function arity
 
     def curried_function(new_given_argument=None):
         if new_given_argument is None:
-            new_given_argument = list()
+            new_given_argument = []
         if function_arity > 1:
             return curry_explicit(
                 function_to_curry,
                 function_arity - 1,
                 given_args=given_args + [new_given_argument],
             )
-        else:
-            if function_arity == 0:
-                return function_to_curry()
-            return function_to_curry(*(given_args + [new_given_argument]))
+        if function_arity == 0:
+            return function_to_curry()
+        return function_to_curry(*given_args, new_given_argument)
 
     return curried_function
 
 
 def uncurry_explicit(function_to_uncurry, function_arity):
     def uncurried_function(*args):
-        if function_arity != len(args) or len(args) == 0:
-            print("Error: Wrong function arity")
-            return None
+        if function_arity != len(args):
+            return None  # Error: wrong function arity
+        if function_arity == 0:
+            return function_to_uncurry()
         res = function_to_uncurry(args[0])
         for arg in args[1:]:
             res = res(arg)
