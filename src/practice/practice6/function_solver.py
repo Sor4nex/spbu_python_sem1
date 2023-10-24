15831529a2cb1c0be9ca55c1d6b01f6913346ccd
Aleksei Ivanov <st117366@student.spbu.ru>
2023-10-24 23:23:59 +0300

    Requested changes added

--- src/practice/practice6/function_solver.py
+++ src/practice/practice6/function_solver.py
@@ -1,60 +1,64 @@
-from typing import Any
+from math import isnan, isinf
 
 
 GREET_USER = "Добро пожаловать в программу для решения квадратных и линейных уравнений"
-INFO_STRING = "Вам нужно ввести 3 числа (A, B, C) через пробел, где A B и C - коэффициенты уравнения Ax^2 + Bx + C = 0(Если нужно решить линейное уравнение, то укажите первым числом 0)"
+INFO_STRING = (
+    "Вам нужно ввести 3 числа (A, B, C) через пробел, где A B и C - коэффициенты уравнения Ax^2 + Bx + C = "
+    "0(Если нужно решить линейное уравнение, то укажите первым числом 0)"
+)
 INPUT_INVITATION = "Введите 3 числа (коэффициенты квадратного уравнения): "
 BAD_INPUT = "Неверный формат!"
 
 
-def is_float_number(input_str: str) -> bool:
-    try:
-        float(input_str)
-    except ValueError:
+def is_float_numbers(input_str: str) -> bool:
+    input_str = input_str.split(" ")
+    if len(input_str) != 3:
         return False
+    for coefficient in input_str:
+        try:
+            float(coefficient.strip())
+        except ValueError:
+            return False
     return True
 
 
-def solve_linear_function(first_coefficient: float, second_coefficient: float) -> Any:
+def solve_function(
+    first_coefficient: float, second_coefficient: float, third_coefficient: float
+) -> tuple[float, ...]:
     if first_coefficient == 0:
         if second_coefficient == 0:
-            return ("корень - любое вещественное число",)
-        return ("корня данного уравнения не существует",)
-    return (-second_coefficient / first_coefficient,)
-
-
-def solve_quadratic_function(
-    first_coefficient: float, second_coefficient: float, third_coefficient: float
-) -> Any:
+            if third_coefficient == 0:
+                raise ValueError("корень - любое вещественное число")
+            raise ValueError("корня данного уравнения не существует")
+        return (-third_coefficient / second_coefficient,)
     discriminant = second_coefficient**2 - 4 * first_coefficient * third_coefficient
     if discriminant < 0:
-        return ("дискриминант уравнения меньше 0, решений нет",)
-    result_1 = (-second_coefficient + (discriminant) ** 0.5) / (2 * first_coefficient)
-    result_2 = (-second_coefficient - (discriminant) ** 0.5) / (2 * first_coefficient)
-    return result_1, result_2
+        raise ValueError("дискриминант уравнения меньше 0, решений нет")
+    elif discriminant == 0:
+        result1 = (-second_coefficient + (discriminant) ** 0.5) / (
+            2 * first_coefficient
+        )
+        return (result1,)
+    result1 = (-second_coefficient + discriminant**0.5) / (2 * first_coefficient)
+    result2 = (-second_coefficient - discriminant**0.5) / (2 * first_coefficient)
+    return result1, result2
 
 
 def main() -> None:
-    print(GREET_USER)
-    print(INFO_STRING)
-    user_input = input(INPUT_INVITATION).split(" ")
-    while len(user_input) != 3 or any(
-        not is_float_number(number) for number in user_input
-    ):
+    print(GREET_USER + "\n" + INFO_STRING)
+    user_input = input(INPUT_INVITATION).strip()
+    if not is_float_numbers(user_input):
         print(BAD_INPUT)
         return None
-    first_num, second_num, third_num = (
-        float(user_input[0]),
-        float(user_input[1]),
-        float(user_input[2]),
-    )
-    if first_num == 0:
-        result = solve_linear_function(second_num, third_num)
-    else:
-        result = solve_quadratic_function(first_num, second_num, third_num)
-        if len(result) > 1 and result[0] == result[1]:
-            result = [result[0]]
-    print(f"Ответ: {' '.join([str(element) for element in result])}")
+    coefficients = [float(number.strip()) for number in user_input.split(" ")]
+    if any(isinf(num) or isnan(num) for num in coefficients):
+        print(BAD_INPUT)
+        return None
+    try:
+        result = solve_function(*coefficients)
+    except ValueError as error:
+        result = [str(error)]
+    print("Ответ:", *result)
 
 
 if __name__ == "__main__":
--- tests/practice/practice6/test_function_solver.py
+++ tests/practice/practice6/test_function_solver.py
@@ -3,69 +3,88 @@ import pytest
 from src.practice.practice6.function_solver import *
 
 
-test_main_parameters = [
-    ("2 -1 -15", "Ответ: 3.0 -2.5"),
-    ("1 -2 1", "Ответ: 1.0"),
-    ("0 1 2", "Ответ: -2.0"),
-    ("0 0 1", "Ответ: корня данного уравнения не существует"),
-    ("0 0 0", "Ответ: корень - любое вещественное число"),
-    ("1 2 3", "Ответ: дискриминант уравнения меньше 0, решений нет"),
-    ("abd     ", f"Ответ: {BAD_INPUT}"),
-    ("2 3", f"Ответ: {BAD_INPUT}"),
-]
-test_float_number_parameters = [
-    ("123", True),
-    ("123.4", True),
-    ("-123", True),
-    ("-123.4", True),
-    ("abc", False),
-]
-test_linear_parameters = [
-    (1, 2, (-2,)),
-    (0, 3, ("корня данного уравнения не существует",)),
-    (0, 0, ("корень - любое вещественное число",)),
-]
-test_quadratic_parametrs = [
-    (1, -2, 1, (1, 1)),
-    (1, 2, 3, ("дискриминант уравнения меньше 0, решений нет",)),
-    (1, -3, 2, (1, 2)),
-    (2, -1, -15, (-2.5, 3)),
-]
-
-
-@pytest.mark.parametrize("test_input,expected", test_float_number_parameters)
-def test_is_float_number(test_input: str, expected: bool) -> None:
-    result = is_float_number(test_input)
+@pytest.mark.parametrize(
+    "test_input,expected",
+    [
+        ("123 123 123", True),
+        ("+123.4 -123.4 25", True),
+        ("-000000.1 21 +33333e-7", True),
+        ("-2e-3 45 45", True),
+        ("nan inf nan", True),
+        ("1,23 1 1", False),
+        ("abc 67", False),
+        ("--34 34 ++34", False),
+        ("3..4 5 5", False),
+        ("d", False),
+        ("  3    4  5", False),
+    ],
+)
+def test_is_float_numbers(test_input: str, expected: bool) -> None:
+    result = is_float_numbers(test_input)
     assert result == expected
 
 
 @pytest.mark.parametrize(
-    "first_argument,second_argument,expected", test_linear_parameters
+    "first_argument,second_argument,third_argument,expected",
+    [(0, 1, 2, (-2,)), (0, 4, -16, (4,)), (0, 5, 0, (0,))],
 )
-def test_solve_linear_function(
-    first_argument: float, second_argument: float, expected
-) -> None:
-    result = solve_linear_function(first_argument, second_argument)
-    assert result == expected
-
-
-@pytest.mark.parametrize(
-    "first_argument,second_argument,third_argument,expected", test_quadratic_parametrs
-)
-def solve_quadratic_function(
+def test_solve_function_linear_valid_cases(
     first_argument: float, second_argument: float, third_argument: float, expected
 ) -> None:
-    result = solve_quadratic_function(first_argument, second_argument, third_argument)
+    result = solve_function(first_argument, second_argument, third_argument)
     assert result == expected
 
 
-@pytest.mark.parametrize("user_input,expected", test_main_parameters)
+@pytest.mark.parametrize(
+    "first_argument,second_argument,third_argument,expected",
+    [
+        (1, -2, 1, (1,)),
+        (1, -3, 2, (1, 2)),
+        (2, -1, -15, (-2.5, 3)),
+    ],
+)
+def solve_function_quadratic_valid_input(
+    first_argument: float, second_argument: float, third_argument: float, expected
+) -> None:
+    result = solve_function(first_argument, second_argument, third_argument)
+    assert result == expected
+
+
+@pytest.mark.parametrize(
+    "first_argument,second_argument,third_argument", [(0, 0, 0), (0, 0, 1), (1, 2, 3)]
+)
+def test_solve_function_exception_cases(
+    first_argument: float, second_argument: float, third_argument: float
+):
+    with pytest.raises(ValueError):
+        solve_function(first_argument, second_argument, third_argument)
+
+
+@pytest.mark.parametrize(
+    "user_input,expected",
+    [
+        ("2 -1 -15", "Ответ: 3.0 -2.5"),
+        ("+4 -5 +1", "Ответ: 1.0 0.25"),
+        ("1 -2 1", "Ответ: 1.0"),
+        ("0 1 2", "Ответ: -2.0"),
+        ("0 0 1", "Ответ: корня данного уравнения не существует"),
+        ("0 0 0", "Ответ: корень - любое вещественное число"),
+        ("1 2 3", "Ответ: дискриминант уравнения меньше 0, решений нет"),
+        ("abd 3 4", BAD_INPUT),
+        ("1 2 3 4", BAD_INPUT),
+        (" abd     ", BAD_INPUT),
+        ("3 inf 3", BAD_INPUT),
+        ("nan 2 3", BAD_INPUT),
+        ("--2 3 3", BAD_INPUT),
+        ("3,3 4 5", BAD_INPUT),
+        ("3.56.3 4 3", BAD_INPUT),
+        ("2 3", BAD_INPUT),
+    ],
+)
 def test_main(monkeypatch, user_input, expected) -> None:
     monkeypatch.setattr("builtins.input", lambda _: user_input)
     fake_output = StringIO()
     monkeypatch.setattr("sys.stdout", fake_output)
     main()
     output = fake_output.getvalue().strip().split("\n")
-    for single_string in output:
-        if single_string not in [GREET_USER, INFO_STRING, INPUT_INVITATION, BAD_INPUT]:
-            assert single_string == expected
+    assert output == [GREET_USER, INFO_STRING, expected]
