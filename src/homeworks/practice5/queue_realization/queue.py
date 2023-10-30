be5a6c15ab0266e7fa29b70dc5afb3df40764756
Aleksei Ivanov <st117366@student.spbu.ru>
2023-10-30 13:07:59 +0300

    Queue datatype is now made on Nodes, not on hashmaps. Some minor comsetics fixes

--- src/homeworks/practice5/queue_realization/queue.py
+++ src/homeworks/practice5/queue_realization/queue.py
@@ -1,47 +1,53 @@
 from dataclasses import dataclass
-from collections import namedtuple
+from typing import Any
+
+
+@dataclass
+class Node:
+    current_value: None
+    next_value: None
 
 
 @dataclass
 class Queue:
     size: int = 0
-    head: dict = None
-    tail: dict = None
+    head: Node = None
+    tail: Node = None
 
 
 def create_new_queue() -> Queue:
     return Queue()
 
 
-def size(given_queue: Queue) -> int:
+def get_size(given_queue: Queue) -> int:
     return given_queue.size
 
 
-def empty(given_queue: Queue) -> bool:
-    return size(given_queue) == 0
+def is_empty(given_queue: Queue) -> bool:
+    return get_size(given_queue) == 0
 
 
-def top(given_queue: Queue) -> any:
-    if empty(given_queue):
+def get_top(given_queue: Queue) -> Any:
+    if is_empty(given_queue):
         return None
-    return given_queue.head["current_value"]
+    return given_queue.head.current_value
 
 
 def push(given_queue: Queue, new_value: any) -> None:
-    new_element = {"current_value": new_value, "next_value": None}
-    if empty(given_queue):
+    new_element = Node(new_value, None)
+    if is_empty(given_queue):
         given_queue.head = new_element
-    elif size(given_queue) == 1:
-        given_queue.head["next_value"] = new_element
+    elif get_size(given_queue) == 1:
+        given_queue.head.next_value = new_element
         given_queue.tail = new_element
     else:
-        given_queue.tail["next_value"] = new_element
+        given_queue.tail.next_value = new_element
         given_queue.tail = new_element
     given_queue.size += 1
 
 
 def pop(given_queue: Queue) -> None:
-    if empty(given_queue):
+    if is_empty(given_queue):
         return None
-    given_queue.head = given_queue.head["next_value"]
+    given_queue.head = given_queue.head.next_value
     given_queue.size -= 1
--- src/homeworks/practice5/queue_realization/usage_example_queue.py
+++ src/homeworks/practice5/queue_realization/usage_example_queue.py
@@ -1,45 +1,49 @@
-from queue import *
+import queue
 
 
-def print_queue_characteristics(given_queue: Queue) -> None:
-    print("\nОчередь пуста:", empty(given_queue))
-    print("Количество элементов очереди:", size(given_queue))
-    print("Первый элемент очереди:", top(given_queue), "\n")
+def print_queue_characteristics(given_queue: queue.Queue) -> None:
+    print("\nОчередь пуста:", queue.is_empty(given_queue))
+    print("Количество элементов очереди:", queue.get_size(given_queue))
+    print("Первый элемент очереди:", queue.get_top(given_queue), "\n")
 
 
 if __name__ == "__main__":
     # создание очереди
-    queue1 = create_new_queue()
+    queue1 = queue.create_new_queue()
     print_queue_characteristics(queue1)
 
     # добавление 1 элемента
-    push(queue1, "hi")
+    queue.push(queue1, "hi")
     print_queue_characteristics(queue1)
 
     # добавление 2 элемента
-    push(queue1, "Mark!")
+    queue.push(queue1, "Mark!")
     print_queue_characteristics(queue1)
 
     # удаление 1 элемента
-    pop(queue1)
+    queue.pop(queue1)
     print_queue_characteristics(queue1)
 
     # добавление 3 элемента
-    push(queue1, "oh hey")
+    queue.push(queue1, "oh hey")
     print_queue_characteristics(queue1)
 
     # добавление 4 элемента
-    push(queue1, "Billy!")
+    queue.push(queue1, "Billy!")
     print_queue_characteristics(queue1)
 
     # удаление 2 элемента
-    pop(queue1)
+    queue.pop(queue1)
     print_queue_characteristics(queue1)
 
     # удаление 3 элемента
-    pop(queue1)
+    queue.pop(queue1)
     print_queue_characteristics(queue1)
 
     # удаление 4 элемента
-    pop(queue1)
+    queue.pop(queue1)
+    print_queue_characteristics(queue1)
+
+    # попытка удаления 5 элемента (его нет)
+    queue.pop(queue1)
     print_queue_characteristics(queue1)
