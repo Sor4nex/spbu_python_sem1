ec2a85390e212d66bdfca2b42550150c376e6cca
Aleksei Ivanov <st117366@student.spbu.ru>
2023-11-12 16:33:43 +0300

    fix: hash function usage. function parametrs. put_value_by_key() and remove_value_by_key() now work faster. Other minor changes.

--- src/practice/practice8/hash.py
+++ src/practice/practice8/hash.py
@@ -25,13 +25,15 @@ class HashMap(Generic[K, V]):
     size: int = 0
 
 
-def count_hash(hash_table_size: int = 100) -> Callable[[K], int]:
-    return lambda key: hash(key) % hash_table_size
+def count_hash(hash_table: HashMap[K, V], key: K) -> int:
+    return hash_table.hash_fn(key) % hash_table.total_buckets
+
+
+def check_load_factor(hash_table: HashMap[K, V]) -> bool:
+    return (hash_table.size / hash_table.total_buckets) >= 0.8
 
 
 def upsize_hash_table(hash_table) -> None:
-    if hash_table.size != hash_table.total_buckets:
-        return None
     all_entries = list(get_items(hash_table))
     old_total = hash_table.total_buckets
     for hash_code in hash_table.used_hashes:
@@ -42,12 +44,11 @@ def upsize_hash_table(hash_table) -> None:
         hash_table.buckets.append(Bucket([], hash_code))
     for entry in all_entries:
         put_value_by_key(hash_table, entry[0], entry[1])
-    hash_table.hash_fn = count_hash(old_total * 2)
 
 
-def create_hash_table(size: int = 100) -> HashMap[K, V]:
-    new_hashmap = HashMap([], [], count_hash())
-    for i in range(size):
+def create_hash_table(hash_function: Callable[[K], int] = hash) -> HashMap[K, V]:
+    new_hashmap = HashMap([], [], hash_function)
+    for i in range(100):
         new_hashmap.buckets.append(Bucket([], i))
     return new_hashmap
 
@@ -59,71 +60,57 @@ def delete_hash_table(hash_table: HashMap[K, V]) -> None:
 
 
 def put_value_by_key(hash_table: HashMap[K, V], key: K, value: V) -> None:
-    hashed_key, entry_by_key = get_entry_by_key(hash_table, key)
+    hashed_key, bucket_len, entry_by_key = get_entry_from_bucket(hash_table, key)
     if entry_by_key is not None:
         remove_by_key(hash_table, key)
-    if hashed_key not in hash_table.used_hashes:
+    if bucket_len == 0:
         hash_table.used_hashes.append(hashed_key)
         hash_table.size += 1
-        hash_table.used_hashes.sort()
     hash_table.buckets[hashed_key].entries.append(Entry(key, value))
     hash_table.buckets[hashed_key].bucket_len += 1
-    upsize_hash_table(hash_table)
+    if check_load_factor(hash_table):
+        upsize_hash_table(hash_table)
 
 
-def get_entry_by_key(
+def get_entry_from_bucket(
     hash_table: HashMap[K, V], key: K
-) -> tuple[int, Optional[tuple[K, V]]]:
-    hashed_key = hash_table.hash_fn(key)
-    if hashed_key not in hash_table.used_hashes:
-        return hashed_key, None
+) -> tuple[int, int, Optional[tuple[K, V]]]:
+    hashed_key = count_hash(hash_table, key)
+    if hash_table.buckets[hashed_key].bucket_len == 0:
+        return hashed_key, 0, None
     needed_bucket = hash_table.buckets[hashed_key]
     for entry in needed_bucket.entries:
         if entry[0] == key:
-            return hashed_key, entry
-    return hashed_key, None
+            return hashed_key, needed_bucket.bucket_len, entry
+    return hashed_key, needed_bucket.bucket_len, None
 
 
 def get_value_by_key(hash_table: HashMap[K, V], key: K) -> V:
-    _, needed_entry = get_entry_by_key(hash_table, key)
+    needed_entry = get_entry_from_bucket(hash_table, key)[2]
     if needed_entry is None:
         raise ValueError(f"key {key} not found in hash-map")
     return needed_entry[1]
 
 
 def has_key(hash_table: HashMap[K, V], key: K) -> bool:
-    _, needed_entry = get_entry_by_key(hash_table, key)
+    needed_entry = get_entry_from_bucket(hash_table, key)[2]
     return needed_entry is not None
 
 
 def remove_by_key(hash_table: HashMap[K, V], key: K) -> V:
-    hashed_key, needed_entry = get_entry_by_key(hash_table, key)
+    hashed_key, bucket_len, needed_entry = get_entry_from_bucket(hash_table, key)
     if needed_entry is None:
         raise ValueError(f"key {key} not found in hash-map")
     hash_table.buckets[hashed_key].entries.remove(needed_entry)
     hash_table.buckets[hashed_key].bucket_len -= 1
-    if hash_table.buckets[hashed_key].bucket_len == 0:
+    if bucket_len == 0:
         hash_table.used_hashes.remove(hashed_key)
         hash_table.size -= 1
-        hash_table.used_hashes.sort()
     return needed_entry[1]
 
 
 def get_items(hash_table: HashMap[K, V]) -> Iterator[tuple[K, V]]:
     for hash_code in hash_table.used_hashes:
         for i in range(hash_table.buckets[hash_code].bucket_len):
-            yield hash_table.buckets[hash_code].entries[i]
-
-
-if __name__ == "__main__":
-    table1 = create_hash_table()
-    table2 = create_hash_table()
-    for i in range(99):
-        put_value_by_key(table1, i, str(i))
-        put_value_by_key(table2, i, str(i))
-    put_value_by_key(table1, 11111111, "Hello, Tim")
-    put_value_by_key(table2, 11111111, "Hello, Tim")
-    print(table1, get_entry_by_key(table1, 11111111))
-    print(table2, get_entry_by_key(table2, 11111111))
-    put_value_by_key(table2, 99, str(99))
-    print(table2, get_entry_by_key(table2, 11111111))
+            entry = hash_table.buckets[hash_code].entries[i]
+            yield entry.key, entry.value
--- tests/practice/practice8/test_hash.py
+++ tests/practice/practice8/test_hash.py
@@ -12,29 +12,31 @@ def dummy_hashmap(input_list: list[tuple[K, V]]) -> HashMap[K, V]:
 @pytest.mark.parametrize(
     "hash_table,key,expected",
     [
-        (dummy_hashmap([(1, "a"), (2, "b"), (3, "c")]), 3, (count_hash()(3), (3, "c"))),
-        (dummy_hashmap([(1, "a"), (2, "b"), (3, "c")]), 4, (count_hash()(4), None)),
+        (dummy_hashmap([(1, "a"), (2, "b"), (3, "c")]), 3, (hash(3) % 100, (3, "c"))),
+        (dummy_hashmap([(1, "a"), (2, "b"), (3, "c")]), 4, (hash(4) % 100, None)),
         (
             dummy_hashmap([("a", 1), ("b", 2), ("c", 3)]),
             "b",
-            (count_hash()("b"), ("b", 2)),
+            (hash("b") % 100, ("b", 2)),
         ),
         (
             dummy_hashmap([((1, 2, 3), "a"), ((2, 3, 4), "b")]),
             (2, 3, 4),
-            (count_hash()((2, 3, 4)), ((2, 3, 4), "b")),
+            (hash((2, 3, 4)) % 100, ((2, 3, 4), "b")),
         ),
         (
             dummy_hashmap([((1, 2, 3), "a"), ((2, 3, 4), "b")]),
             (2, 3, 4, 5),
-            (count_hash()((2, 3, 4, 5)), None),
+            (hash((2, 3, 4, 5)) % 100, None),
         ),
     ],
 )
 def test_get_entry_by_key(
     hash_table: HashMap[K, V], key: K, expected: tuple[int, Optional[tuple[K, V]]]
 ) -> None:
-    result = get_entry_by_key(hash_table, key)
+    result = get_entry_from_bucket(hash_table, key)[::2]
+    if result[1] is not None:
+        result = (result[0], (result[1].key, result[1].value))
     assert result == expected
 
 
@@ -112,7 +114,7 @@ def test_get_value_by_key_expception_case(hash_table: HashMap[K, V], key: K) -> 
             [(1, "A"), (2, "b"), (5, "aaaa")],
         ),
         (dummy_hashmap([(1, "a")]), 1, "b", [(1, "b")]),
-        (dummy_hashmap([(1, "a"), (3, "b")]), 2, "c", [(1, "a"), (2, "c"), (3, "b")]),
+        (dummy_hashmap([(1, "a"), (3, "b")]), 2, "c", [(1, "a"), (3, "b"), (2, "c")]),
         (
             dummy_hashmap([((1, 2, 3), "a")]),
             (4, 5, 6, 7),
@@ -167,14 +169,14 @@ def test_remove_by_key_expection_case(hash_table: HashMap[K, V], key: K) -> None
 def test_resize_hash_table() -> None:
     table1 = create_hash_table()
     table2 = create_hash_table()
-    for i in range(99):
+    for i in range(79):
         put_value_by_key(table1, i, str(i))
         put_value_by_key(table2, i, str(i))
     put_value_by_key(table1, 11111111, "Hello, Tim")
     put_value_by_key(table2, 11111111, "Hello, Tim")
-    put_value_by_key(table2, 99, str(99))
+    put_value_by_key(table2, 79, str(79))
     assert (
         len(table1.buckets) != len(table2.buckets)
-        and get_entry_by_key(table1, 11111111)[0]
-        != get_entry_by_key(table2, 11111111)[0]
+        and get_entry_from_bucket(table1, 11111111)[0]
+        != get_entry_from_bucket(table2, 11111111)[0]
     )
